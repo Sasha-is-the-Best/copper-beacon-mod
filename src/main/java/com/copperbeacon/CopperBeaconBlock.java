da49031ccd07177b00d84df1311570438be68011
Sasha-is-the-best <fs1212751@gmail.com>
2026-06-24 12:54:42 +0200

    Update CopperBeaconBlock.java

--- src/main/java/com/copperbeacon/CopperBeaconBlock.java
+++ src/main/java/com/copperbeacon/CopperBeaconBlock.java
@@ -1,26 +1,13 @@
 package com.copperbeacon;
 
-import com.mojang.serialization.MapCodec;
-import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
-public class CopperBeaconBlock extends BlockWithEntity {
-    public static final MapCodec<CopperBeaconBlock> CODEC = createCodec(CopperBeaconBlock::new);
-
-    public CopperBeaconBlock(Settings settings) { super(settings); }
-
-    @Override
-    protected MapCodec<? extends BlockWithEntity> getCodec() { return CODEC; }
-
-    @Override
-    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new CopperBeaconBlockEntity(pos, state);
+public class CopperBeaconBlock extends Block {
+    public CopperBeaconBlock(Settings settings) {
+        super(settings);
     }
-
-    @Override
-    public BlockRenderType getRenderType(BlockState state) { return BlockRenderType.MODEL; }
 }

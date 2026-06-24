354e9e838ac2ab7534eb7199527372f7fa854f10
Sasha-is-the-best <fs1212751@gmail.com>
2026-06-24 12:55:27 +0200

    Update CopperBeaconBlockEntity.java

--- src/main/java/com/copperbeacon/CopperBeaconBlockEntity.java
+++ src/main/java/com/copperbeacon/CopperBeaconBlockEntity.java
@@ -1,25 +1,12 @@
 package com.copperbeacon;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.entity.BeaconBlockEntity;
-import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.block.BlockState;
 
-public class CopperBeaconBlockEntity extends BeaconBlockEntity {
+public class CopperBeaconBlockEntity extends BlockEntity {
     public CopperBeaconBlockEntity(BlockPos pos, BlockState state) {
-        super(pos, state);
-    }
-
-    @Override
-    public boolean isValidBlock(World world, int x, int y, int z) {
-        BlockState state = world.getBlockState(pos.add(x, y, z));
-        
-        // Accetta tutti i tipi di rame + varianti ossidate + cerate
-        if (state.getBlock().getName().getString().toLowerCase().contains("copper")) return true;
-        
-        // Fallback: accetta anche ferro, oro, smeraldo, diamante, netherite come vanilla
-        return state.isIn(BlockTags.BEACON_BASE_BLOCKS);
+        super(ModBlockEntities.COPPER_BEACON_BE, pos, state);
     }
 }
